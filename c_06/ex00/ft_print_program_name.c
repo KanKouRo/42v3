bc72de84d9434636dce815cf007d87e261567dbf
Nicolas <gomisnicolas@live.fr>
2021-02-24 14:51:41 +0100

    add c_06

---
+++ c_06/ex00/ft_print_program_name.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/20 09:36:25 by ngomis            #+#    #+#             */
+/*   Updated: 2021/02/20 11:04:38 by ngomis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_putstr(argv[0]);
+	write(1, "\n", 1);
+	return (0);
+}
