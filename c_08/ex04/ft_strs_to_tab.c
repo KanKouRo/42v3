5cb004666f8d9f61e4088a21bdb23f4e47240b19
Nicolas <gomisnicolas@live.fr>
2021-02-25 12:46:23 +0100

    c08

--- c_08/ex04/ft_strs_to_tab.c
+++ c_08/ex04/ft_strs_to_tab.c
@@ -6,14 +6,62 @@
 /*   By: ngomis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 15:18:10 by ngomis            #+#    #+#             */
-/*   Updated: 2021/02/24 17:02:38 by ngomis           ###   ########.fr       */
+/*   Updated: 2021/02/25 12:17:54 by ngomis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "ft_stock_str.h"
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+
+int					ft_strlen(char *str)
 {
-	s_stock_str *s;
-	
-	if (!(*s = malloc(sizeof(s_stock_str) * )	
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char				*ft_strdup(char *src)
+{
+	char	*str;
+	int		j;
+	int		i;
+
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	str = malloc(sizeof(str) * (j + 1));
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*t;
+	int			i;
+
+	if (ac < 0)
+		ac = 0;
+	if (!(t = malloc((ac + 1) * sizeof(t_stock_str))))
+		return (NULL);
+	i = -1;
+	while (++i < ac)
+	{
+		t[i].size = ft_strlen(av[i]);
+		t[i].str = av[i];
+		t[i].copy = ft_strdup(av[i]);
+	}
+	t[i].str = 0;
+	return (t);
+}
