46aa08ed2c4359b0890acc54ceb57dc4dbdc77c1
Nicolas <gomisnicolas@live.fr>
2021-03-22 14:23:12 +0100

    c04 ex04 ok ex05 pas ok

--- c_04/ex04/ft_putnbr_base.c
+++ c_04/ex04/ft_putnbr_base.c
@@ -1,31 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/20 13:55:00 by ngomis            #+#    #+#             */
+/*   Updated: 2021/03/22 14:14:33 by ngomis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-int        ft_erreur(char *base)
+int		ft_erreur(char *base)
 {
-    int i;
-    int n;
+	int i;
+	int n;
 
-    i = 1;
-    n = 0;
-    if (base[n] == '\0' || base[n + 1] == '\0')
-        return (1);
-    while (base[n] != '\0')
-    {
-        while (base[i] != '\0')
-        {
-            if (base[n] == base[i] || (base[n] == '-' || base[n] == '+'))
-                return (1);
-            i++;
-        }
-        n++;
-        i = (n + 1);
-    }
-    return (0);
+	i = 1;
+	n = 0;
+	if (base[n] == '\0' || base[n + 1] == '\0')
+		return (1);
+	while (base[n] != '\0')
+	{
+		while (base[i] != '\0')
+		{
+			if (base[n] == base[i] || (base[n] == '-' || base[n] == '+')
+					|| (base[i] == '-' || base[i] == '+'))
+				return (1);
+			i++;
+		}
+		n++;
+		i = (n + 1);
+	}
+	return (0);
 }
 
 void	ft_putnbr(unsigned int n, char *base, unsigned int size)
@@ -35,34 +48,27 @@ void	ft_putnbr(unsigned int n, char *base, unsigned int size)
 		ft_putnbr(n / size, base, size);
 		n %= size;
 	}
-	ft_putchar(base[n]);
+	ft_putchar(base[n % size]);
 }
 
-void    ft_putnbr_base(int nbr, char *base)
+void	ft_putnbr_base(int nbr, char *base)
 {
-    unsigned int i;
-    int size;
+	unsigned int	i;
+	int				size;
 
-    size = 0;
-    i = 0;
-    while (base[size] != '\0')
-        size++;
-    if (ft_erreur(base) == 1)
-        return ;
-    if (nbr < 0)
-    {
-        ft_putchar('-');
-        ft_putnbr(-nbr, base, size);
-    }
-    else
-    {
-        ft_putnbr(nbr, base, size);
-    }
-}
-
-#include <stdio.h>
-int main ()
-{
-    ft_putnbr_base(0, "0123456789ABCDEF");
-    return (0);
+	size = 0;
+	i = 0;
+	while (base[size] != '\0')
+		size++;
+	if (ft_erreur(base) == 1)
+		return ;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nbr, base, size);
+	}
+	else
+	{
+		ft_putnbr(nbr, base, size);
+	}
 }
--- c_04/ex05/ft_atoi_base.c
+++ c_04/ex05/ft_atoi_base.c
@@ -1,4 +1,16 @@
-int	ft_atoi(char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/20 17:02:10 by ngomis            #+#    #+#             */
+/*   Updated: 2021/03/20 17:33:48 by ngomis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_atoi(char *str)
 {
 	int i;
 	int nbr;
@@ -17,4 +29,68 @@ int	ft_atoi(char *str)
 	if (sign)
 		nbr *= -1;
 	return (nbr);
+}
+
+int		ft_erreur(char *base)
+{
+	int i;
+	int n;
+
+	i = 1;
+	n = 0;
+	if (base[n] == '\0' || base[n + 1] == '\0')
+		return (1);
+	while (base[n] != '\0')
+	{
+		while (base[i] != '\0')
+		{
+			if ((base[n] == base[i] || (base[n] == '-' || base[n] == '+')
+						|| (base[n] >= 9 && base[n] <= 13) || base[n] == ' ')
+						|| (base[i] >= 9 && base[i] <= 13) || base[i] == ' ')
+				return (1);
+			i++;
+		}
+		n++;
+		i = (n + 1);
+	}
+	return (0);
+}
+
+void	ft_putnbr(unsigned int n, char *base, unsigned int size)
+{
+	if (n > size - 1)
+	{
+		ft_putnbr(n / size, base, size);
+		n %= size;
+	}
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	unsigned int	i;
+	int				size;
+
+	size = 0;
+	i = 0;
+	while (base[size] != '\0')
+		size++;
+	if (ft_erreur(base) == 1)
+		return ;
+	if (nbr < 0)
+	{
+		ft_putnbr(-nbr, base, size);
+	}
+	else
+	{
+		ft_putnbr(nbr, base, size);
+	}
+}
+
+int		ft_atoi_base(char *str, char *base)
+{
+	long int i;
+
+	i = ft_atoi(str);
+	ft_putnbr_base(i, base);
+	return (i);
 }
